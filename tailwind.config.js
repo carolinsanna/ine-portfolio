87acd749abd312460b1721b944098474437805c4
Carolina Dias <carolinatdias@live.com>
2024-11-14 18:23:41 -0300

    update: config tailwind colorset

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ine: {
+          gray: {
+            100: '#b3b3b3',
+            200: '#e5e5e5',
+            300: '#5a5a5a',
+            400: '#919191',
+            500: '#757575'
+          },
+          black:{
+            100: '#21272a',
+            200: '#1e1e1e'
+          }
+        }
+
+      }
+    },
   },
   plugins: [],
 }
