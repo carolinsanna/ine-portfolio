440936bd7177142bfcc27e20f373233b199b64f4
Carolina Dias <carolinatdias@live.com>
2024-11-15 00:57:18 -0300

    update: add components to renderer

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,12 +5,10 @@ import { Button } from './../../components/Button'
 export function Home(){
   return (
     <div className='flex flex-col items-center gap-4'>
+
       <div className="px-20 max-w-screen-xl">
-        <header>
-          <Hero
-            hello = 'Hello, my name is Inessa'
-            description = "I'm a passionate UX/UI Designer with 5+ years of experience in digital products"
-          />
+        <header className='flex flex-col'>
+          <Hero/>
           <Button
             link='/About'
             value='More About Me'
