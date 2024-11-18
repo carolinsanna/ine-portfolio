3ccad770fed47853b91b57f1e1ace5772320a3e0
Carolina Dias <carolinatdias@live.com>
2024-11-18 19:34:38 -0300

    update: change logo

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,8 +1,8 @@
-import Logo from "./../../assets/logo.svg?react"
+import Logo from "./../../assets/logo-light.svg?react"
 export const Nav = () => {
     return (			
     	<nav className="flex justify-between py-5 px-20 sticky top-0 z-40 backdrop-blur transition-colors duration-500 max-w-screen-xl mx-auto">
-				<Logo className="-mx-6 self-center"/>
+				<Logo className="self-center"/>
 				<ul className="flex gap-4 text-ine-black-100 font-semibold">
 					<li className="px-2 py-3 self-center"><a href="/Home" target="_blank" rel="noopener noreferrer">Home</a></li>
 					<li className="px-2 py-3 self-center"><a href="/Work" target="_blank" rel="noopener noreferrer">Work</a></li>
