4a4dbb7032d9cf24a20319f3907061d397f48bd0
Carolina Dias <carolinatdias@live.com>
2024-11-18 19:37:23 -0300

    update: include Nav components to main page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,16 @@
 import { Hero } from './../../components/Hero'
 import { Work } from './../../components/Work'
 import { Button } from './../../components/Button'
+import { Nav } from './../../components/Nav'
 
 export function Home(){
   return (
-    <div className='flex flex-col items-center gap-4'>
-
+    <div className='flex flex-col items-center gap-4 w-full'>
+      <div className=' sticky top-0 z-40 backdrop-blur transition-colors duration-500 w-full'>
+        <Nav/>
+      </div>
       <div className="px-20 max-w-screen-xl">
-        <header className='flex flex-col'>
+        <header className='flex flex-col'>          
           <Hero/>
           <Button
             link='/About'
@@ -18,6 +21,7 @@ export function Home(){
           <Work />
         </main>        
       </div>
+
     </div>
   )
 }
