7120fc8928c377f589a1015643fe81f831b14350
Carolina Dias <carolinatdias@live.com>
2024-11-18 19:39:02 -0300

    update: include Nav components to main page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,7 @@ import { Nav } from './../../components/Nav'
 export function Home(){
   return (
     <div className='flex flex-col items-center gap-4 w-full'>
-      <div className=' sticky top-0 z-40 backdrop-blur transition-colors duration-500 w-full'>
+      <div className='sticky top-0 z-40 backdrop-blur transition-colors duration-500 w-full'>
         <Nav/>
       </div>
       <div className="px-20 max-w-screen-xl">
