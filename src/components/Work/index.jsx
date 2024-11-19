cf890a0dd6bbb543f9c4cd767dd14da406e0fb40
Carolina Dias <carolinatdias@live.com>
2024-11-18 21:41:27 -0300

    update: component attr

--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -1,13 +1,7 @@
 import { Card } from './../../components/Card'
-import { Button } from './../../components/Button'
 
 export const Work = () => {
-  return (
-    <section className="flex flex-col items-center gap-8 py-20">
-			<div>
-				<p className='text-lg font-bold text-center'>Here is some of my</p>
-				<h2 className="text-[2.75rem] font-bold text-center">Professional Work</h2>
-			</div>
+  return (    
       <div className="flex gap-4 justify-center">
         <Card
           imgsrc="/placeholder.png"
@@ -28,10 +22,5 @@ export const Work = () => {
           link="/Title"
         />
       </div>
-			<Button
-        link='/Work'
-        value='See All'
-      />
-    </section>
   )
 }
