c4cb679089bb1eeb8fcc6f5be9fe6a80565ef9a0
Carolina Dias <carolinatdias@live.com>
2024-11-23 19:50:21 -0300

    fix: links text accessibilty

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,10 +5,10 @@ export const Footer = () => {
 		return (
 			<footer className="py-12 flex flex-col gap-10">
 				<div className="flex gap-10 justify-center">
-					<a href="https://www.linkedin.com/in/inessadl/" target="_blank" rel="noopener noreferrer">
+					<a href="https://www.linkedin.com/in/inessadl/" target="_blank" rel="noopener noreferrer" aria-label="Inessa Luerce Linkedin">
 						<Linkedin/>
 					</a>
-					<a href="https://www.instagram.com/inessadl/" target="_blank" rel="noopener noreferrer">
+					<a href="https://www.instagram.com/inessadl/" target="_blank" rel="noopener noreferrer" aria-label="Inessa Luerce Instagram">
 						<Instagram/>
 					</a>
 				</div>
