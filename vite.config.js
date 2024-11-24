197c075e966e8fceb0f1f433729393e77a9b3c8b
Carolina Dias <carolinatdias@live.com>
2024-11-23 21:05:27 -0300

    fix: react-router-dom for vercel

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true,
+  }
 })
