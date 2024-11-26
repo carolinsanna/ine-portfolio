959cc6eba158f2f8b002e9b7999933ca5a2a1379
Carolina Dias <carolinatdias@live.com>
2024-11-26 20:54:57 -0300

    update: cards thumbs

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -40,35 +40,35 @@ export function Home(){
             />
             <div className="grid gap-4 justify-center sm:grid-cols-2 lg:grid-cols-3">
               <Card
-                imgsrc="/nursa-bill-pay.png"
+                imgsrc="/nursa-bill-pay-thumb.png"
                 title="Billings and Payments"
                 description="Targeted micro-projects for Billings and Payments team at Nursa"
                 link="/Title"
                 alt="Bilings and Payments"
               />
               <Card
-                imgsrc="/nursa-manage-requests.png"
+                imgsrc="/nursa-manage-requests-thumb.png"
                 title="Manage Requests"
                 description="A panel that helps schedulers to select which nurse is the best fit for a job"
                 link="/Title"
                 alt="Manage Requests"
               />
               <Card
-                imgsrc="/nursa-resume-imports.png"
+                imgsrc="/nursa-resume-imports-thumb.png"
                 title="Resume Importer"
                 description="Automatically fills out user's profile by uploading their resume"
                 link="/Title"
                 alt="Resume Importer"
               />
               <Card
-                imgsrc="/pse.png"
+                imgsrc="/pse-thumb.png"
                 title="Plataforma Socioeducativa"
                 description="Logo and the Design System for the Plataforma Socioeducativa (PSE)"
                 link="/Title"
                 alt="Plataforma Socioeducativa"
               />
               <Card
-                imgsrc="/wodz.png"
+                imgsrc="/wodz-thumb.png"
                 title="Wodz"
                 description="Logo for the Music Schools and Studio Wodz"
                 link="/Title"
@@ -83,42 +83,42 @@ export function Home(){
             />
             <div className="grid gap-4 justify-center sm:grid-cols-2 lg:grid-cols-3">
               <Card
-                imgsrc="/veg.png"
+                imgsrc="/veg-thumb.png"
                 title="Guia Veg (WIP)"
                 description="Current phase: ideation"
                 link="/Title"
                 alt="Guia Veg"
               />
               <Card
-                imgsrc="/catsis.png"
+                imgsrc="/catsis-thumb.png"
                 title="CatSis"
                 description="An app to find someone to help you with taking care of your cat"
                 link="/Title"
                 alt="CatSis"
               />
               <Card
-                imgsrc="/food-app.png"
+                imgsrc="/food-app-thumb.png"
                 title="Food / Drink App"
                 description="Daily UI Challenge: Food / Drink Menu"
                 link="/Title"
                 alt="Food / Drink App"
               />
               <Card
-                imgsrc="/404.png"
+                imgsrc="/404-thumb.png"
                 title="404 Page"
                 description="Daily Challenge: 404 Page"
                 link="/Title"
                 alt="404 Page"
               />
               <Card
-                imgsrc="/portfolio.png"
+                imgsrc="/portfolio-thumb.png"
                 title="Portfolio"
                 description="Process of making this portfolio"
                 link="/Home"
                 alt="Portfolio"
               />
               <Card
-                imgsrc="/credit-card.png"
+                imgsrc="/credit-card-thumb.png"
                 title="Credit Card"
                 description="Daily UI Challenge: Credit Card"
                 link="/Title"
